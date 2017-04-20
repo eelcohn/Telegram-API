e6656b6b85a049df91995fb9f6a9cc750fbd24df
eelcohn <eelco@huininga.nl>
2017-04-20 18:31:09 +0200

    Repaired the image searcher (still a bit buggy though)

--- ImageSearch.tcl
+++ ImageSearch.tcl
@@ -1,14 +1,14 @@
 # ---------------------------------------------------------------------------- #
 # Image Search module v0.1 for Eggdrop with the Telegram-API module            #
 #                                                                              #
-# written by Eelco Huininga 2016                                               #
+# written by Eelco Huininga 2016-2017                                          #
 # ---------------------------------------------------------------------------- #
 
 # ---------------------------------------------------------------------------- #
 # Configuration settings                                                       #
 # ---------------------------------------------------------------------------- #
 
-
+# Safe Search: -2 for 'Off', -1 for 'Average', 0 for 'On'
 
 # ---------------------------------------------------------------------------- #
 # Image Search procedures                                                      #
@@ -17,14 +17,23 @@
 # ---------------------------------------------------------------------------- #
 
 proc imagesearch_getImage {chat_id msgid channel message parameter_start} {
+	set s_region nl_nl
+	set s_language nl_NL
+	set s_safesearch -2
+
 	tg_sendChatAction $chat_id "upload_photo"
 
 	set imagequery [string range $message $parameter_start end]
-	set imgresult [exec curl --tlsv1.2 -s -X GET https://duckduckgo.com/i.js?kah=nl-nl&kl=nl-nl&kad=nl_NL&kp=-1&q=$imagequery]
+	set imgresult [exec curl --tlsv1.2 -s -X GET https://api.duckduckgo.com/?kah=nl-nl&kl=$s_region&kad=$s_language&kp=$s_safesearch&q=$imagequery]
 	# Bug: the object should really be "message" and not ""
-	set url [jsonGetValue $imgresult "" "image"]
-	set title [jsonGetValue $imgresult "" "title"]
+	set url [jsonGetValue $imgresult "" "Image"]
+	set title [jsonGetValue $imgresult "" "Abstract"]
 
-	tg_sendPhoto $chat_id "$msgid" "$url" "$title"
-	putchan $channel "[strip_html $url]"
+	if {$url == ""} {
+		tg_sendMessage $chat_id "html" "Nothing found :-("
+		putchan $channel "Nothing found :-("
+	} else {
+		tg_sendPhoto $chat_id "$msgid" "$url" "$title"
+		putchan $channel "[strip_html $url]"
+	}
 }
