05faf40af4c93405686cecdbfb017413ba35e67c
eelcohn <eelco@huininga.nl>
2017-04-25 21:00:27 +0200

    Enclosed curl-command in a catch statement for catching network timeouts

--- Soundcloud.tcl
+++ Soundcloud.tcl
@@ -20,8 +20,12 @@ proc soundcloud_getTrack {chat_id msgid channel message parameter_start} {
 	tg_sendChatAction $chat_id "typing"
 
 	set query [string map {" " "%20"} [string trim [string range $message $parameter_start end]]]
-	set result [exec curl --tlsv1.2 -s -X GET https://api.soundcloud.com/tracks.json?client_id=4346c8125f4f5c40ad666bacd8e96498&q=$query&limit=1]
-
+	if { [ catch {
+		set result [exec curl --tlsv1.2 -s -X GET https://api.soundcloud.com/tracks.json?client_id=4346c8125f4f5c40ad666bacd8e96498&q=$query&limit=1]
+	} ] } {
+		putlog "Soundcloud.tcl: cannot connect to api.soundcloud.com using tracks.json method: $result"
+		return -1
+	}
 	set url [jsonGetValue $result "" "permalink_url"]
 
 	tg_sendReplyToMessage $chat_id $msgid "html" "$url"
