53c2fc2fd905086edfc8bc6aa2b3294b8e38b689
eelcohn <eelco@huininga.nl>
2017-11-02 15:11:24 +0100

    Fixed displaying result when no location is found

--- Locate.tcl
+++ Locate.tcl
@@ -30,14 +30,12 @@ proc openstreetmaps_getLocation {chat_id msgid channel message parameter_start} 
 	set result [string map {" : " ":"} $result]
 	set display_name [jsonGetValue $result "" "display_name"]
 	if {$display_name eq ""} {
-		set display_name "Nothing found."
-		set lat ""
-		set lon ""
+		tg_sendReplyToMessage $chat_id $msgid "html" "Nothing found."
+		putchan $channel "Nothing found."
 	} else {
 		set lat [jsonGetValue $result "" "lat"]
 		set lon [jsonGetValue $result "" "lon"]
+		tg_sendVenue $chat_id $msgid $lat $lon $locationquery $display_name
+		putchan $channel "[strip_html $display_name] is at $lat $lon"
 	}
-
-	tg_sendReplyToMessage $chat_id $msgid "html" "$lat $lon $display_name"
-	putchan $channel "[strip_html $display_name] is at $lat $lon"
 }
