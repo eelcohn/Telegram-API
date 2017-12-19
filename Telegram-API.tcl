50844370873c52e0ff2547946f5d56bad24f723f
eelcohn <eelco@huininga.nl>
2017-12-19 18:57:48 +0100

    Fixed colorizing nicknames for all message types

--- Telegram-API.tcl
+++ Telegram-API.tcl
@@ -174,7 +174,7 @@ proc irc2tg_modeChange {nick uhost hand channel mode target} {
 # Poll the Telegram server for updates                                         #
 # ---------------------------------------------------------------------------- #
 proc tg2irc_pollTelegram {} {
-	global tg_bot_id tg_bot_token tg_update_id tg_poll_freq tg_channels utftable irc_botname
+	global tg_bot_id tg_bot_token tg_update_id tg_poll_freq tg_channels utftable irc_botname colorize_nicknames
 	global MSG_TG_MSGSENT MSG_TG_AUDIOSENT MSG_TG_PHOTOSENT MSG_TG_DOCSENT MSG_TG_STICKERSENT MSG_TG_VIDEOSENT MSG_TG_VOICESENT MSG_TG_CONTACTSENT MSG_TG_LOCATIONSENT MSG_TG_VENUESENT MSG_TG_USERADD MSG_TG_USERLEFT MSG_TG_CHATTITLE MSG_TG_PICCHANGE MSG_TG_PICDELETE MSG_TG_UNIMPL
 
 	set result [libtelegram::getUpdates $tg_update_id]
