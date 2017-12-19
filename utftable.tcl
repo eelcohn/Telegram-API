a436d6772061d8f1b7f8acbb0afbbba1c82e93a3
eelcohn <eelco@huininga.nl>
2017-12-19 20:00:34 +0100

    Added \ud83e\udd23 :rofl:

--- utftable.tcl
+++ utftable.tcl
@@ -699,6 +699,7 @@ array set utftable {
 	{\\ud83e\\udd17}	{" :hugging_face:"}
 	{\\ud83e\\udd18}	{" :hands_hornsign:"}
 	{\\ud83e\\udd21}	{" :clown:"}
+	{\\ud83e\\udd23}	{" "rofl:"}
 	{\\udbb8\\uddc4}	{" :monkey_face:"}
 	{\\uddba\\udf1a}	{" :hearts:"}
 	{\\uddba\\udf1c}	{" :diamonds:"}
