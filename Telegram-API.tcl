c7567616c3cb369521ddb8ebc9b92e9f0cc3c401
eelcohn <eelco@huininga.nl>
2017-12-19 18:00:42 +0100

    Fixed website preview on IRC

--- Telegram-API.tcl
+++ Telegram-API.tcl
@@ -226,7 +226,7 @@ proc tg2irc_pollTelegram {} {
 						if {$chatid eq $tg_chat_id} {
 							foreach line [split [string map {\\n \n} $txt] "\n"] {
 								putchan $irc_channel [format $MSG_TG_MSGSENT "\003[getColorFromString $name][utf2ascii $name]\003" "[remove_slashes $line]"]
-								if {[string first $line "http://"] || [string first $line "https://"]} {
+								if {[string first "http://" $line] || [string first "https://" $line]} {
 									putchan $irc_channel [getWebsiteTitle $line]
 								}
 							}
