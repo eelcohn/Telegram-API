3a14ffa0d68ae362be0b99ef92f1e7b857c59b14
eelcohn <eelco@huininga.nl>
2018-01-19 09:02:59 +0100

    Added support for forwarded messages

--- lang/Telegram-API.dutch.tcl
+++ lang/Telegram-API.dutch.tcl
@@ -1,5 +1,5 @@
 # ---------------------------------------------------------------------------- #
-# Telegram-API module for Eggdrop - Dutch language file v20180118              #
+# Telegram-API module for Eggdrop - Dutch language file v20180119              #
 #                                                                              #
 # written by Eelco Huininga 2016-2018                                          #
 # ---------------------------------------------------------------------------- #
@@ -17,6 +17,7 @@ set MSG_IRC_MODECHANGE		"<i>%1\$s@IRC</i> zet de modus %3\$s op %2\$s"
 # Messages from Telegram to IRC
 set MSG_TG_MSGSENT		"%1\$s@Telegram: %2\$s"
 set MSG_TG_MSGREPLYTOSENT "%1\$s (als antwoord op %2\$s@Telegram: %3\$s)"
+set MSG_TG_MSGFORWARDED "%1\$s (doorgestuurd van %2\$s)"
 set MSG_TG_AUDIOSENT		"%1\$s@Telegram heeft een audiobestand verstuurd: %2\$s - %3\$s (%4\$s s). Gebruik /ctcp %5\$s tgfile %6\$s om het bestand te downloaden."
 set MSG_TG_DOCSENT		"%1\$s@Telegram heeft een document verstuurd: %2\$s (%3\$s bytes). Gebruik /ctcp %4\$s tgfile %5\$s om het bestand te downloaden."
 set MSG_TG_PHOTOSENT		"%1\$s@Telegram heeft een foto verstuurd%2\$s. Gebruik /ctcp %3\$s tgfile %4\$s om het bestand te downloaden."
