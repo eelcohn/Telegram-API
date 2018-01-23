830dd233d2f45fd2bc21f4c794fa504e56b1a44c
eelcohn <eelco@huininga.nl>
2018-01-23 14:21:40 +0100

    Update PSN.tcl

--- modules/PSN.tcl
+++ modules/PSN.tcl
@@ -8,7 +8,7 @@
 # Configuration settings                                                       #
 # ---------------------------------------------------------------------------- #
 
-
+source "[file join [file dirname [info script]] PSN.conf]"
 
 # ---------------------------------------------------------------------------- #
 # Playstation Network procedures                                               #
