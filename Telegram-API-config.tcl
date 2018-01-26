e097100c468caf962add3ba180014d7aa29d0288
eelcohn <eelco@huininga.nl>
2018-01-26 10:22:15 +0100

    Moved all config settings to the ::telegram namespace

--- Telegram-API-config.tcl
+++ Telegram-API-config.tcl
@@ -1,5 +1,5 @@
 # ---------------------------------------------------------------------------- #
-# Telegram-API configuration v20180124 for Eggdrop                             #
+# Telegram-API configuration v20180126 for Eggdrop                             #
 #                                                                              #
 # written by Eelco Huininga 2016-2018                                          #
 # ---------------------------------------------------------------------------- #
@@ -7,17 +7,17 @@
 # ---------------------------------------------------------------------------- #
 # Configuration settings                                                       #
 # ---------------------------------------------------------------------------- #
-set ::libtelegram::bot_id	123456789
-set ::libtelegram::bot_token	AABCD-EfGhIj_KlMn_OpQrStUvWxYz12345
-set tg_poll_freq		5
-set tg_web_page_preview		false
-set locale			"en"
-set timeformat			"%Y-%m-%d %H:%M:%S"
-set colorize_nicknames		"true"
-set ::telegram::cfg::userflags	"jlvck"
-set ::telegram::cfg::cmdmodifier	"/"
+set ::libtelegram::bot_id		123456789
+set ::libtelegram::bot_token		AABCD-EfGhIj_KlMn_OpQrStUvWxYz12345
+set ::telegram::tg_poll_freq		5
+set ::telegram::tg_web_page_preview	false
+set ::telegram::locale			"en"
+set ::telegram::timeformat		"%Y-%m-%d %H:%M:%S"
+set ::telegram::colorize_nicknames	"true"
+set ::telegram::userflags		"jlvck"
+set ::telegram::cmdmodifier		"/"
 
-array set tg_channels {
+array set ::telegram::tg_channels {
 	"-21436587"		"#lamer"
 	"-171615141"		"#lamer-test"
 }
