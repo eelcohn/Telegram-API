fc335759dbf46c04a440ff4ab4231a39a3a4727a
eelcohn <eelco@huininga.nl>
2018-01-28 17:16:13 +0100

    Update Stickers.tcl

--- Stickers.tcl
+++ Stickers.tcl
@@ -1,37 +1,38 @@
 array set stickertable {
+	# Great minds stickerset
 	{CAADBAADGQADyIsGAAFl6KYZBflVyQI}	{"Abraham Lincoln - https://telegram.org/file/811140066/1/7fM-CwKk4F0/53f9f1fc731c63547d"}
 	{CAADBAADIQADyIsGAAHaCFln7THl9QI}	{"Albert Einstein - https://telegram.org/file/811140981/1/9ypk7z_8T4Q/4381a7a74256fe0b9b"}
 	{CAADBAADOAADyIsGAAE7re09I3hMQwI}	{"Arthur C. Doyle - "}
-  {CAADBAADKQADyIsGAAEPYMQ7iT8jRgI} {"Audrey Hepburn - https://telegram.org/file/811140508/1/0BiCdZ28uVw/ca64f37939b1bdea11"}
+	{CAADBAADKQADyIsGAAEPYMQ7iT8jRgI}	{"Audrey Hepburn - https://telegram.org/file/811140508/1/0BiCdZ28uVw/ca64f37939b1bdea11"}
 	{CAADBAADPAADyIsGAAHP7udlFGeCVgI}	{"Benjamin Franklin - "}
 	{CAADBAADJQADyIsGAAGoEDksgR1WpAI}	{"Bob Marley - https://telegram.org/file/811140235/2/_ZthaAI89Ds/4a2f85c6feac9e1bca"}
-  {CAADBAADPQADyIsGAAER4mZIkRHRsgI} {"Bruce Lee - "}
+	{CAADBAADPQADyIsGAAER4mZIkRHRsgI}	{"Bruce Lee - "}
 	{CAADBAADHQADyIsGAAFsLeimr-HBPAI}	{"Charlie Chaplin - https://telegram.org/file/811140437/2/VdpCps5zB1o/c906dac5e81170dc74"}
 	{CAADBAADQwADyIsGAAHtlVE1rDyZXQI}	{"Che Guevaris - "}
 	{CAADBAADJAADyIsGAAGbgy1wIY571wI}	{"Cleopatra - https://telegram.org/file/811140617/2/JdFfZi4Ruhk/3444e8ea6e1a88ace4"}
-  {CAADBAADKAADyIsGAAHTcQFJMaFa0QI}	{"Coco Chanel - https://telegram.org/file/811140860/1/_sVhXePrr-I/02a6e44c917d7c7f6f"}
+	{CAADBAADKAADyIsGAAHTcQFJMaFa0QI}	{"Coco Chanel - https://telegram.org/file/811140860/1/_sVhXePrr-I/02a6e44c917d7c7f6f"}
 	{CAADBAADMAADyIsGAAEq-fQsNrRXjAI}	{"Dante Alighieri - https://telegram.org/file/811140444/1/NqWMRLWzg1U/623bd278ff0179ded9"}
 	{CAADBAADIwADyIsGAAHeuQrNOU12cgI}	{"Edgar Allan Poe - https://telegram.org/file/811140654/1/xFdm58wPNH4/191814f44f5d36f9a4"}
 	{CAADBAADNgADyIsGAAFUgH2PcO7e6QI}	{"Edvard Munch - "}
 	{CAADBAADPwADyIsGAAGI1S2AmQ002AI}	{"Elvis Presley - "}
-  {CAADBAADNwADyIsGAAFOgk-EJUfH-gI} {"Ervin Shryodinger - "}
+	{CAADBAADNwADyIsGAAFOgk-EJUfH-gI}	{"Ervin Shryodinger - "}
 	{CAADBAADFgADyIsGAAGo-jfilnNJyAI}	{"Franz Kafka - https://telegram.org/file/811140332/2/KJzzslK440U/b11181fd7bbf1c56aa"}
 	{CAADBAADQAADyIsGAAE7MpzFPFQX5QI}	{"Freddy Mercury - "}
-  {CAADBAADMgADyIsGAAGw4osRBXRB3AI} {"Frederik Douglas - "} 
+	{CAADBAADMgADyIsGAAGw4osRBXRB3AI}	{"Frederik Douglas - "} 
 	{CAADBAADMwADyIsGAAFI6xgyK-qNMgI}	{"George Carlin - "}
-  {CAADBAADRQADyIsGAAH1EiOQIkWtagI} {"George Orwell - "}
+	{CAADBAADRQADyIsGAAH1EiOQIkWtagI}	{"George Orwell - "}
 	{CAADBAADNAADyIsGAAGjDT0cxHKm2gI}	{"George Washington - "}
-  {CAADBAADOQADyIsGAAG3aA9Q2a6wywI} {"Howard Lavkraft - "}
-  {CAADBAADNQADyIsGAAF_Q3DFvKDCzgI} {"Isaac Newton - "}
+	{CAADBAADOQADyIsGAAG3aA9Q2a6wywI}	{"Howard Lavkraft - "}
+	{CAADBAADNQADyIsGAAF_Q3DFvKDCzgI}	{"Isaac Newton - "}
 	{CAADBAADLQADyIsGAAGJcalkB0bXSQI}	{"Jay Hawkins - https://telegram.org/file/811140885/1/r4SVbb90Quc/7333932a5fdcf5f057"}
 	{CAADBAADLgADyIsGAAGPsGcNmlLjPQI}	{"Jimi Hendrix - https://telegram.org/file/811140525/2/vCDL8Gv_k6U/a39c95e5aa621c46c6"}
-  {CAADBAADOgADyIsGAAH3hdtO3aqUpwI} {"Jeanne d'Arc - "}
+	{CAADBAADOgADyIsGAAH3hdtO3aqUpwI}	{"Jeanne d'Arc - "}
 	{CAADBAADGAADyIsGAAGsLFQJpxof0QI}	{"John Lennon - https://telegram.org/file/811140517/1/yDccYORy60c/da6c69de6ad5afcb8f"}
 	{CAADBAADFAADyIsGAAFXtQ3Z4ZqdLAI}	{"J.R.R. Tolkien - https://telegram.org/file/811140679/1/qyTW5F98KLA/ed2db7cc24b8938e07"}
 	{CAADBAADEgADyIsGAAE-gRk5Wrs8NwI}	{"Julius Cesar approves - https://telegram.org/file/811140007/2/uHbXgsdVXQY/a75eee858dd829fb89"}
-  {CAADBAADIAADyIsGAAGwI-I5pMSEdQI} {"Julius Cesar disapproves - https://telegram.org/file/811140801/1/14Ka_5LSkus/11ef924b87783bd339"}
+	{CAADBAADIAADyIsGAAGwI-I5pMSEdQI}	{"Julius Cesar disapproves - https://telegram.org/file/811140801/1/14Ka_5LSkus/11ef924b87783bd339"}
 	{CAADBAADPgADyIsGAAEuCrQ7AXgedwI}	{"Kurt Cobain - "}
-  {CAADBAADLAADyIsGAAF6cRm5hiOkCAI} {"Lady Diana - "}
+	{CAADBAADLAADyIsGAAF6cRm5hiOkCAI}	{"Lady Diana - "}
 	{CAADBAADRgADyIsGAAF-kAhqVu7-1QI}	{"Leopold von Sacher-Masoch - "}
 	{CAADBAADQQADyIsGAAHv8OkTOGk0SwI}	{"Louis Armstrong - "}
 	{CAADBAADGwADyIsGAAE68wt0DU90mgI}	{"Mahatma Gandhi - https://telegram.org/file/811140053/1/A6JVcXDZiZM/d28d4a5029d010e953"}
@@ -39,14 +40,14 @@ array set stickertable {
 	{CAADBAADEAADyIsGAAFCZre3kULkngI}	{"Marilyn Monroe - https://telegram.org/file/811140117/2/dGMVFePbDos/68ae9b4e90b10c1fb8"}
 	{CAADBAADHwADyIsGAAFHId3ggt-PAAEC}	{"Mark Twain - https://telegram.org/file/811140849/1/cpP6i93xxaA/c082e8a2ad59522cfd"}
 	{CAADBAADMQADyIsGAAEX-8KrVIT5lwI}	{"Napoleon Bonaparte - https://telegram.org/file/811140354/1/-NUBye754Pw/2fa1e3148047b624b3"}
-  {CAADBAADFQADyIsGAAHPdbDyCcpB8gI} {"Nikola Tesla - "}
+	{CAADBAADFQADyIsGAAHPdbDyCcpB8gI}	{"Nikola Tesla - "}
 	{CAADBAADEwADyIsGAAHgGKfPXeaQDAI}	{"Richard Wagner - "}
-  {CAADBAADHAADyIsGAAFzjQavel2uswI}	{"Salvador Dali - https://telegram.org/file/811140579/1/i974MzakdcM/7752f8ee0262a57fdd"}
-  {CAADBAADGgADyIsGAAHw_WAnR5mwUAI} {"Siegmund Freud - "}
-  {CAADBAADFwADyIsGAAF1CK9t7qjAigI} {"Steve Jobs - https://telegram.org/file/811140823/2/gEXq-ZISs9g/55f6c9c9f87c4dceb0"}
-  {CAADBAADJwADyIsGAAH6wu4EMqyrvgI} {"Virginia Vulf - "}
-  {CAADBAADNQADyIsGAAF_Q3DFvKDCzgI} {"Walt Disney - "}
-  {CAADBAADQgADyIsGAAHbkbbtFVHNZAI} {"Whitney Houston - "}
-  {CAADBAADOwADyIsGAAFClAdwTFmgJwI} {"Wilhelm Konrad Rontgen - "}
-  {CAADBAADRwADyIsGAAGQU-a9UvB-xQI} {"William Shakespear - "}
+	{CAADBAADHAADyIsGAAFzjQavel2uswI}	{"Salvador Dali - https://telegram.org/file/811140579/1/i974MzakdcM/7752f8ee0262a57fdd"}
+	{CAADBAADGgADyIsGAAHw_WAnR5mwUAI}	{"Siegmund Freud - "}
+	{CAADBAADFwADyIsGAAF1CK9t7qjAigI}	{"Steve Jobs - https://telegram.org/file/811140823/2/gEXq-ZISs9g/55f6c9c9f87c4dceb0"}
+	{CAADBAADJwADyIsGAAH6wu4EMqyrvgI}	{"Virginia Vulf - "}
+	{CAADBAADNQADyIsGAAF_Q3DFvKDCzgI}	{"Walt Disney - "}
+	{CAADBAADQgADyIsGAAHbkbbtFVHNZAI}	{"Whitney Houston - "}
+	{CAADBAADOwADyIsGAAFClAdwTFmgJwI}	{"Wilhelm Konrad Rontgen - "}
+	{CAADBAADRwADyIsGAAGQU-a9UvB-xQI}	{"William Shakespear - "}
 }
