809b6c5f17868f107c723041926be6015c794333
eelcohn <eelco@huininga.nl>
2018-01-28 14:38:26 +0100

    Updated colorization of nicknames to match colors in the Telegram-desktop

--- Telegram-API-config.tcl
+++ Telegram-API-config.tcl
@@ -1,5 +1,5 @@
 # ---------------------------------------------------------------------------- #
-# Telegram-API configuration v20180126 for Eggdrop                             #
+# Telegram-API configuration v20180128 for Eggdrop                             #
 #                                                                              #
 # written by Eelco Huininga 2016-2018                                          #
 # ---------------------------------------------------------------------------- #
@@ -13,10 +13,16 @@ set ::telegram::tg_poll_freq		5
 set ::telegram::tg_web_page_preview	false
 set ::telegram::locale			"en"
 set ::telegram::timeformat		"%Y-%m-%d %H:%M:%S"
-set ::telegram::colorize_nicknames	"true"
+set ::telegram::colorize_nicknames	7
 set ::telegram::userflags		"jlvck"
 set ::telegram::cmdmodifier		"/"
-
+array set	::telegram::usercolors	{	0	4
+						1	7
+						2	6
+						3	3
+						4	10
+						5	2
+						6	13}
 array set ::telegram::tg_channels {
 	"-21436587"		"#lamer"
 	"-171615141"		"#lamer-test"
