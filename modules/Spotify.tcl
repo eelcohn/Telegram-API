27820bb4eabe0923f7a51512b7bd2362e96fb1be
eelcohn <eelco@huininga.nl>
2018-02-10 17:15:21 +0100

    Update Spotify.tcl

--- modules/Spotify.tcl
+++ modules/Spotify.tcl
@@ -1,5 +1,5 @@
 # ---------------------------------------------------------------------------- #
-# Spotify module for Eggdrop with the Telegram-API module v20180201            #
+# Spotify module for Eggdrop with the Telegram-API module v20180210            #
 #                                                                              #
 # written by Eelco Huininga 2016-2018                                          #
 # ---------------------------------------------------------------------------- #
@@ -39,5 +39,5 @@ proc spotify_getTrack {chat_id msgid channel message parameter_start} {
 	putchan $channel "[strip_html $url]"
 }
 
-add_public_command spotify spotify_getTrack "<keyword>: Search Spotify for music matching <keyword>."
+::telegram::addPublicCommand spotify spotify_getTrack "<keyword>: Search Spotify for music matching <keyword>."
 
