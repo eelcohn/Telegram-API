68df44867b6ac361af26951e03db6fefa12e0042
eelcohn <eelco@huininga.nl>
2018-02-11 14:06:48 +0100

    Added localized help messages

--- modules/Locate.tcl
+++ modules/Locate.tcl
@@ -1,5 +1,5 @@
 # ---------------------------------------------------------------------------- #
-# Locate module for Eggdrop with the Telegram-API module v20180201             #
+# Locate module for Eggdrop with the Telegram-API module v20180211             #
 #                                                                              #
 # written by Eelco Huininga 2017-2018                                          #
 # ---------------------------------------------------------------------------- #
@@ -8,7 +8,7 @@
 # Configuration settings                                                       #
 # ---------------------------------------------------------------------------- #
 
-
+source "[file join [file dirname [info script]] Locate.conf]"
 
 # ---------------------------------------------------------------------------- #
 # OpenStreetMaps procedures                                                    #
@@ -17,25 +17,29 @@
 # ---------------------------------------------------------------------------- #
 
 proc openstreetmaps_getLocation {chat_id msgid channel message parameter_start} {
-	set locationquery [string map {" " "%20"} [string trim [string range $message $parameter_start end]]]
-	if { [ catch {
-		set result [exec curl --tlsv1.2 -s -X GET "https://nominatim.openstreetmap.org/search?q=$locationquery&format=json&polygon=0&addressdetails=0"]
-	} ] } {
-		putlog "Locate.tcl: cannot connect to api.openstreetmaps.com using search method."
-		return -1
-	}
+	if {[set locationquery [string map {" " "%20"} [string trim [string range $message $parameter_start end]]]]} {
+		if { [ catch {
+			set result [exec curl --tlsv1.2 -s -X GET "https://nominatim.openstreetmap.org/search?q=$locationquery&format=json&polygon=0&addressdetails=0"]
+		} ] } {
+			putlog "Locate.tcl: cannot connect to api.openstreetmaps.com using search method."
+			return -1
+		}
 
-	set result [string map {" : " ":"} $result]
-	set display_name [::libjson::getValue $result ".\[0\].display_name"]
-	if {$display_name eq ""} {
-		libtelegram::sendReplyToMessage $chat_id $msgid "html" "Nothing found."
-		putchan $channel "Nothing found."
+		set result [string map {" : " ":"} $result]
+		set display_name [::libjson::getValue $result ".\[0\].display_name"]
+		if {$display_name eq ""} {
+			libtelegram::sendReplyToMessage $chat_id $msgid "html" "Nothing found."
+			putchan $channel "Nothing found."
+		} else {
+			set lat [::libjson::getValue $result ".\[0\].lat"]
+			set lon [::libjson::getValue $result ".\[0\].lon"]
+			libtelegram::sendVenue $chat_id $msgid $lat $lon $locationquery $display_name
+			putchan $channel "[strip_html $display_name]: https://www.openstreetmap.org/#map=20/$lat/$lon"
+		}
+		return 0
 	} else {
-		set lat [::libjson::getValue $result ".\[0\].lat"]
-		set lon [::libjson::getValue $result ".\[0\].lon"]
-		libtelegram::sendVenue $chat_id $msgid $lat $lon $locationquery $display_name
-		putchan $channel "[strip_html $display_name]: https://www.openstreetmap.org/#map=20/$lat/$lon"
+		return -1
 	}
 }
 
-::telegram::addPublicCommand locate openstreetmaps_getLocation "<keyword>: Search for location <keyword> on OpenStreetMap."
+::telegram::addPublicCommand locate openstreetmaps_getLocation "[::msgcat::mc MSG_LOCATE_HELP]"
