b916c58cf1e4ff7eb9260daa49e8f5b9c22a3431
eelcohn <eelco@huininga.nl>
2018-02-11 14:48:19 +0100

    Added localization for help messages

--- modules/Spotify.tcl
+++ modules/Spotify.tcl
@@ -8,7 +8,7 @@
 # Configuration settings                                                       #
 # ---------------------------------------------------------------------------- #
 
-
+source "[file join [file dirname [info script]] Spotify.conf]"
 
 # ---------------------------------------------------------------------------- #
 # Spotify procedures                                                           #
@@ -17,27 +17,31 @@
 # ---------------------------------------------------------------------------- #
 
 proc spotify_getTrack {chat_id msgid channel message parameter_start} {
-	set spotifyquery [string map {" " "%20"} [string trim [string range $message $parameter_start end]]]
-	if { [ catch {
-		set result [exec curl --tlsv1.2 -s -X GET https://api.spotify.com/v1/search?q=$spotifyquery&type=track&limit=1]
-	} ] } {
-		putlog "Spotify.tcl: cannot connect to api.spotify.com using search method."
+	if {[set spotifyquery [string map {" " "%20"} [string trim [string range $message $parameter_start end]]]] ne ""} {
+		if { [ catch {
+			set result [exec curl --tlsv1.2 -s -X GET https://api.spotify.com/v1/search?q=$spotifyquery&type=track&limit=1]
+		} ] } {
+			putlog "Spotify.tcl: cannot connect to api.spotify.com using search method."
+			return -1
+		}
+
+		if {[::libjson::hasKey $result ".error"]} {
+			set url "Error [::libjson::getValue $result ".error.status"] [::libjson::getValue $result ".error.message"]"
+		} else {
+			if {[::libjson::getValue $result "total"] eq "0"} {
+				set url "[::msgcat::mc MSG_SPOTIFY_NOTFOUND]"
+			} else {
+				set url [::libjson::getValue $result "spotify"]
+			}
+		}
+
+		::libtelegram::sendMessage $chat_id $msgid "html" "$url"
+		putchan $channel "[strip_html $url]"
+
+		return 0
+	} else {
 		return -1
 	}
-
-	if {[::libjson::hasKey $result ".error"]} {
-		set url "Error [::libjson::getValue $result ".error.status"] [::libjson::getValue $result ".error.message"]"
-	} else {
-		if {[::libjson::getValue $result "total"] eq "0"} {
-			set url "Nothing found."
-		} else {
-			set url [::libjson::getValue $result "spotify"]
-		}
-	}
-
-	::libtelegram::sendMessage $chat_id $msgid "html" "$url"
-	putchan $channel "[strip_html $url]"
 }
 
-::telegram::addPublicCommand spotify spotify_getTrack "<keyword>: Search Spotify for music matching <keyword>."
-
+::telegram::addPublicCommand spotify spotify_getTrack "[::msgcat::mc MSG_SPOTIFY_HELP]"
