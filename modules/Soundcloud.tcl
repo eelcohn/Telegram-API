ac08c04d6cceb2ac5eafab7b54947be616a33890
eelcohn <eelco@huininga.nl>
2018-02-11 14:44:46 +0100

    Added localization for help message

--- modules/Soundcloud.tcl
+++ modules/Soundcloud.tcl
@@ -1,5 +1,5 @@
 # ---------------------------------------------------------------------------- #
-# Soundcloud module for Eggdrop with the Telegram-API module v20180210         #
+# Soundcloud module for Eggdrop with the Telegram-API module v20180211         #
 #                                                                              #
 # written by Eelco Huininga 2016-2018                                          #
 # ---------------------------------------------------------------------------- #
@@ -8,7 +8,7 @@
 # Configuration settings                                                       #
 # ---------------------------------------------------------------------------- #
 
-
+source "[file join [file dirname [info script]] SoundCloud.conf]"
 
 # ---------------------------------------------------------------------------- #
 # Soundcloud procedures                                                        #
@@ -17,22 +17,27 @@
 # ---------------------------------------------------------------------------- #
 
 proc soundcloud_getTrack {chat_id msgid channel message parameter_start} {
-	set query [string map {" " "%20"} [string trim [string range $message $parameter_start end]]]
-	if { [ catch {
-		set result [exec curl --tlsv1.2 -s -X GET https://api.soundcloud.com/tracks.json?client_id=4346c8125f4f5c40ad666bacd8e96498&q=$query&limit=1]
-	} ] } {
-		putlog "Soundcloud.tcl: cannot connect to api.soundcloud.com using tracks.json method."
+	if {[set query [string map {" " "%20"} [string trim [string range $message $parameter_start end]]]] ne ""} {
+		if { [ catch {
+			set result [exec curl --tlsv1.2 -s -X GET https://api.soundcloud.com/tracks.json?client_id=4346c8125f4f5c40ad666bacd8e96498&q=$query&limit=1]
+		} ] } {
+			putlog "Soundcloud.tcl: cannot connect to api.soundcloud.com using tracks.json method."
+			return -1
+		}
+
+		if {[::libjson::getValue $result "total"] eq "0"} {
+			set url "[::msgcat::mc MSG_SOUNDCLOUD_NOTFOUND]"
+		} else {
+			set url [::libjson::getValue $result "permalink_url"]
+		}
+
+		::libtelegram::sendMessage $chat_id $msgid "html" "$url"
+		putchan $channel "[strip_html $url]"
+
+		return 0
+	} else {
 		return -1
 	}
-
-	if {[::libjson::getValue $result "total"] eq "0"} {
-		set url "Nothing found."
-	} else {
-		set url [::libjson::getValue $result "permalink_url"]
-	}
-
-	::libtelegram::sendMessage $chat_id $msgid "html" "$url"
-	putchan $channel "[strip_html $url]"
 }
 
-::telegram::addPublicCommand soundcloud soundcloud_getTrack "<keyword>: Search SoundCloud for music matching <keyword>."
+::telegram::addPublicCommand soundcloud soundcloud_getTrack "[::msgcat::mc MSG_SOUNDCLOUD_HELP]"
