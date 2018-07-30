d2dedc53bc7a3fae9a74e0db3b811a3bc3d4cc6d
eelcohn <eelco@huininga.nl>
2018-07-30 22:13:31 +0200

    Updated sendMessage calls to v4.0 (July 26, 2018)

--- modules/IRChelper.tcl
+++ modules/IRChelper.tcl
@@ -1,5 +1,5 @@
 # ---------------------------------------------------------------------------- #
-# IRC helper module for Eggdrop with the Telegram-API module v20180212         #
+# IRC helper module for Eggdrop with the Telegram-API module v20180730         #
 #                                                                              #
 # written by Eelco Huininga 2016-2018                                          #
 # ---------------------------------------------------------------------------- #
