fde7b4bd4c7acab72e584667ab6726ad63878858
eelcohn <eelco@huininga.nl>
2018-08-03 14:29:51 +0200

    Update ImageSearch.tcl

--- modules/ImageSearch.tcl
+++ modules/ImageSearch.tcl
@@ -18,7 +18,7 @@ source "[file join [file dirname [info script]] ImageSearch.conf]"
 # ---------------------------------------------------------------------------- #
 
 proc ::ImageSearch::getImage {from_id chat_id msgid channel message parameter_start} {
-	if {[set imagequery [string trim ]string range $message $parameter_start end]]] ne ""} {
+	if {[set imagequery [string trim [string range $message $parameter_start end]]] ne ""} {
 		if { [ catch {
 #			set imgresult [exec curl --tlsv1.2 -s -X POST https://api.duckduckgo.com/ -d kah=nl-nl -d kl=$s_region -d kad=$s_language -d kp=$s_safesearch -d q=$imagequery]
 #			set imgresult [exec curl --tlsv1.2 -s -X GET https://api.duckduckgo.com/?kah=nl-nl&kl=$s_region&kad=$s_language&kp=$s_safesearch&q=$imagequery]
@@ -28,7 +28,7 @@ proc ::ImageSearch::getImage {from_id chat_id msgid channel message parameter_st
 			set imgresult ""
 		}
 
-		if {[set status [::libjson::getValue $imgresult ".status//empty"]] != "success"} {
+		if {[set status [::libjson::getValue $imgresult ".status//empty"]] ne "success"} {
 			set error_code [::libjson::getValue $imgresult ".data.error_code//empty"]
 			set reply [::msgcat::mc MSG_IMAGESEARCH_ERROR "$status" "$error_code"]
 			::libtelegram::sendMessage $chat_id "$reply" "html" false $msgid "" 
