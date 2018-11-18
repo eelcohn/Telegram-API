fb45b8504607710c8e5265e5a90e3baa905aab3e
eelcohn <eelco@huininga.nl>
2018-11-18 22:27:51 +0100

    Added ::telegram::serveraddress variable

--- Telegram-API-config.tcl
+++ Telegram-API-config.tcl
@@ -1,5 +1,5 @@
 # ---------------------------------------------------------------------------- #
-# Telegram-API configuration v20180211 for Eggdrop                             #
+# Telegram-API configuration v20181118 for Eggdrop                             #
 #                                                                              #
 # written by Eelco Huininga 2016-2018                                          #
 # ---------------------------------------------------------------------------- #
@@ -9,6 +9,7 @@
 # ---------------------------------------------------------------------------- #
 set	::libtelegram::bot_id		123456789
 set	::libtelegram::bot_token	AABCD-EfGhIj_KlMn_OpQrStUvWxYz12345
+set	::telegram::serveraddress	irc.freenode.net
 set	::telegram::tg_poll_freq	5
 set	::telegram::tg_web_page_preview	false
 set	::telegram::tg_prefer_usernames true
